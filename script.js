57729304833045888cea8048a3fbf617f9a0b1f6
karul <180303552+karulll@users.noreply.github.com>
2026-04-20 15:01:13 +0800

    Impelement gameBoard functions to be used in game controller

--- script.js
+++ script.js
@@ -2,20 +2,44 @@
 function gameBoard() {
 	// -> define the rows and cols and the 2d board array itself
 	const rows = 3;
-	const col = 3;
+	const cols = 3;
 	const board = [];
 
+	// -> have a for loop to fill that 2d board using .push method filling it with cells which are objects that contain the player and that players value
 	for (let i = 0; i < rows; i++) {
 		board[i] = [];
-		for (let j = 0; i < col; j++) {
-			board.push(Cell());
+		for (let j = 0; j < cols; j++) {
+			board[i].push(Cell());
 		}
 	}
+
+	// -> have a method to get the current state of the board
+	const getBoard = () => board;
+
+	// -> have a method method add a players value to a cell, check if that cell is available first (3x3 grid)
+	const setPlayerValue = (row, column, player) => {
+		// safeguardingS
+		if (row < 0 || row > 2 || column < 0 || column > 2) {
+			return false;
+		}
+
+		// check if that cell is available first
+		if (board[row][column].getValue() === 0) {
+			board[row][column].addValue(player);
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	// -> have a method to print the current state of the board
+	const printBoard = () => {
+		const currentBoard = board.map((row) => row.map((cell) => cell.getValue()));
+		console.table(currentBoard);
+	};
+
+	return { getBoard, setPlayerValue, printBoard };
 }
-// -> have a for loop to fill that 2d board using .push method filling it with cells which are objects that contain the player and that players value
-// -> have a method to get the current state of the board
-// -> have a method method to check if the cell a user picked is valid or invalid, place their value there if valid, else return
-// -> have a method to print the current state of the board
 
 // I need a constructor for the cells, return methods: get the value inside the cell and add the value to the cell, figure it out later
 function Cell() {
@@ -23,23 +47,13 @@ function Cell() {
 	let value = 0;
 
 	// method for adding the players value to the cell
-	const addPlayerValue = (playerValue) => {
+	const addValue = (playerValue) => {
 		value = playerValue;
 	};
 
 	// method for sending / getting the current value of the cell
-
 	const getValue = () => value;
 
-	return { addPlayerValue, getValue };
+	return { addValue, getValue };
 }
-// -> use factory functions instead and make use of private variables and functions
-
-// now assuming we have every method to be able to simulate a real game, we make a controller for the game
-// first we need to define our players and their values
-// define a variable for the game board to get access to its methods
-// have a method to get the current active player
-// have a method to switch the active player per round
-// have a method to print the current round, the positions, active player etc
-// have a method to play the round
 // have a method to check the current state of the board and determine if a player has a winning pattern respective of their values
